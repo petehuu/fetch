1f25a4a89c38b2f147faec8dd2d7b455e1ab5eb1
petehuu <petehuu@gmail.com>
2025-02-06 23:05:55 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');  // Import the cors package
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());  // Use the cors middleware
 
 // Tarjotaan staattiset tiedostot root-hakemistosta
 app.use(express.static(path.join(__dirname, '/')));
