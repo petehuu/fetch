9a4b7700285d121af5c4a663faceab779b84ca0c
petehuu <petehuu@users.noreply.github.com>
2025-02-06 21:45:42 +0000

    deploy: dbd255d7e20b4e4180d37055db87ab6051880999

--- express-server.js
+++ express-server.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Allow CORS for all routes for debugging purposes
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-}));
+// Set CORS headers for all responses
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
-// Tarjotaan staattiset tiedostot root-hakemistosta
+// Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '/')));
 
-// Reitti lukemaan status.json
+// Route to read status.json
 app.get('/status', (req, res) => {
-    console.log('GET /status called');
     fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading file:', err);
@@ -32,9 +29,8 @@ app.get('/status', (req, res) => {
     });
 });
 
-// Reitti päivittämään status.json
+// Route to update status.json
 app.post('/update-status', (req, res) => {
-    console.log('POST /update-status called');
     const newStatus = req.body;
     fs.writeFile(path.join(__dirname, 'status.json'), JSON.stringify(newStatus, null, 2), (err) => {
         if (err) {
