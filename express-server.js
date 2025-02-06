74049bbe70e2425f9f153f370b819ea8c1bc6669
petehuu <petehuu@users.noreply.github.com>
2025-02-06 21:49:06 +0000

    deploy: 46940ea88e6fc42bf32959aeda69de2bc48975da

--- express-server.js
+++ express-server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,8 +10,11 @@ app.use(express.json());
 // Set CORS headers for all responses
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     next();
 });
 
@@ -19,6 +23,7 @@ app.use(express.static(path.join(__dirname, '/')));
 
 // Route to read status.json
 app.get('/status', (req, res) => {
+    console.log('GET /status called');
     fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading file:', err);
@@ -31,6 +36,7 @@ app.get('/status', (req, res) => {
 
 // Route to update status.json
 app.post('/update-status', (req, res) => {
+    console.log('POST /update-status called');
     const newStatus = req.body;
     fs.writeFile(path.join(__dirname, 'status.json'), JSON.stringify(newStatus, null, 2), (err) => {
         if (err) {
