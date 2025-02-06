52866a06938ed83ac007b6a03349818fa08f4155
petehuu <petehuu@users.noreply.github.com>
2025-02-06 22:05:02 +0000

    deploy: 29ac27d09299b16a933cc09f37a38bcf13adaec6

--- proxy-server.js
+++ proxy-server.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());  // Use the cors middleware
 
+// Middleware to set CORS headers manually
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
+
 // Proxy endpoint to forward requests to your Express server
 app.use('/api', createProxyMiddleware({
     target: 'http://localhost:3000',  // Your Express server
