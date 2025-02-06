706fceae31bff2512725d2bf00ac2312cfbc5865
petehuu <petehuu@users.noreply.github.com>
2025-02-06 21:41:02 +0000

    deploy: 7da5d0a985d4bad940ca61818e4752c617155895

--- express-server.js
+++ express-server.js
@@ -6,21 +6,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors()); // Use the cors middleware
+
+// Allow CORS for all routes for debugging purposes
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}));
 
 // Tarjotaan staattiset tiedostot root-hakemistosta
 app.use(express.static(path.join(__dirname, '/')));
 
-// Middleware to set CORS headers manually
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');  // Allow specific methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 // Reitti lukemaan status.json
 app.get('/status', (req, res) => {
+    console.log('GET /status called');
     fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading file:', err);
@@ -33,6 +34,7 @@ app.get('/status', (req, res) => {
 
 // Reitti päivittämään status.json
 app.post('/update-status', (req, res) => {
+    console.log('POST /update-status called');
     const newStatus = req.body;
     fs.writeFile(path.join(__dirname, 'status.json'), JSON.stringify(newStatus, null, 2), (err) => {
         if (err) {
