1e04acfa823c424c92f9c34d54cb9be1d3fbcb5c
petehuu <petehuu@gmail.com>
2025-02-07 20:52:15 +0200

    Update test-websocket-client.js

--- test-websocket-client.js
+++ test-websocket-client.js
@@ -1,6 +1,8 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Ohitetaan TLS-sertifikaatin tarkistus (vain kehityskäytössä)
+
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:80');
+const ws = new WebSocket('wss://localhost:3000'); // Käytetään WebSocket-yhteyttä HTTPS:llä
 
 ws.on('open', function open() {
     console.log('WebSocket-yhteys avattu');
@@ -8,14 +10,15 @@ ws.on('open', function open() {
 });
 
 ws.on('message', function message(data) {
-    console.log('Vastaanotettu:', data);
+    const parsedData = JSON.parse(data);
+    console.log('Vastaanotettu:', parsedData);
     ws.close();  // Sulje WebSocket-yhteys viestin vastaanottamisen jälkeen
 });
 
-ws.on('error', function error(error) {
-    console.error('WebSocket-virhe:', error);
+ws.on('error', function error(e) {
+    console.error('WebSocket-virhe:', e);
 });
 
-ws.on('close', function close(code, reason) {
-    console.log(`WebSocket-yhteys suljettu. Koodi: ${code}, Syy: ${reason}`);
+ws.on('close', function close() {
+    console.log('WebSocket-yhteys suljettu');
 });
