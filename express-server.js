bcb4db305de794fc9acd409a2fbbc04c97ba0967
petehuu <petehuu@gmail.com>
2025-02-07 20:49:49 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const https = require('https');
-const SockJS = require('sockjs');
+const WebSocket = require('ws');
 
 const app = express();
 const port = 3000; // Käytetään porttia 3000
@@ -14,43 +14,39 @@ app.use(cors());
 // Aseta staattiset tiedostot
 app.use(express.static(path.join(__dirname, '/')));
 
-// SockJS-palvelin
-const sockjs = SockJS.createServer();
-sockjs.on('connection', (conn) => {
-    console.log('SockJS-yhteys muodostettu');
-    conn.write(JSON.stringify({ message: 'Yhteys muodostettu' })); // Lähetetään vahvistus JSON-muodossa
-
-    conn.on('data', (message) => {
-        console.log('Vastaanotettu:', message);
-        // Käsittele SockJS-viesti ja vastaa
-        if (message === 'getStatus') {
-            fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
-                if (err) {
-                    console.error('Virhe luettaessa tiedostoa:', err);
-                    conn.write(JSON.stringify({ status: 'error', message: 'Virhe luettaessa tiedostoa' }));
-                } else {
-                    conn.write(data);
-                }
-            });
-        }
-    });
-
-    conn.on('error', (error) => {
-        console.error('SockJS-virhe:', error);
-    });
-
-    conn.on('close', () => {
-        console.log('SockJS-yhteys suljettu');
-    });
-});
-
 // Lataa SSL-sertifikaatti ja avain
 const privateKey = fs.readFileSync('server.key', 'utf8');
 const certificate = fs.readFileSync('server.cert', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
 const server = https.createServer(credentials, app);
-sockjs.installHandlers(server, { prefix: '/websocket' });
+
+// WebSocket-palvelin
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('WebSocket-yhteys muodostettu');
+    ws.send(JSON.stringify({ message: 'Yhteys muodostettu' })); // Lähetetään vahvistus JSON-muodossa
+
+    ws.on('message', (message) => {
+        console.log('Vastaanotettu:', message);
+        // Käsittele WebSocket-viesti ja vastaa
+        if (message === 'getStatus') {
+            fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Virhe luettaessa tiedostoa:', err);
+                    ws.send(JSON.stringify({ status: 'error', message: 'Virhe luettaessa tiedostoa' }));
+                } else {
+                    ws.send(data);
+                }
+            });
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket-yhteys suljettu');
+    });
+});
 
 // Lisää `/status` reitti
 app.get('/status', (req, res) => {
