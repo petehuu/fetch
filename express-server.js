f5e6c5b5302a16c5da6b1b9dc66daeb7cddc255f
petehuu <petehuu@users.noreply.github.com>
2025-02-07 08:33:01 +0000

    deploy: 59d2d876ddae08b1465329e0b1ada69d360f4ae8

--- express-server.js
+++ express-server.js
@@ -5,49 +5,22 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(cors());  // Käytä cors-middlewarea
 
-// Set CORS headers for all responses
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-    next();
-});
-
-// Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '/')));
 
-// Route to read status.json
 app.get('/status', (req, res) => {
-    console.log('GET /status called');
+    res.setHeader('Access-Control-Allow-Origin', '*');  // Salli pyynnöt mistä tahansa alkuperästä
     fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
         if (err) {
-            console.error('Error reading file:', err);
-            res.status(500).send('Error reading file');
+            console.error('Virhe luettaessa tiedostoa:', err);
+            res.status(500).send('Virhe luettaessa tiedostoa');
         } else {
             res.json(JSON.parse(data));
         }
     });
 });
 
-// Route to update status.json
-app.post('/update-status', (req, res) => {
-    console.log('POST /update-status called');
-    const newStatus = req.body;
-    fs.writeFile(path.join(__dirname, 'status.json'), JSON.stringify(newStatus, null, 2), (err) => {
-        if (err) {
-            console.error('Error writing file:', err);
-            res.status(500).send('Error writing file');
-        } else {
-            res.send('File updated successfully');
-        }
-    });
-});
-
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Serveri käynnissä osoitteessa http://localhost:${port}`);
 });
