d7f3148e05321b77f6116119271107cfbe010c54
petehuu <petehuu@gmail.com>
2025-02-07 13:09:54 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -41,6 +41,19 @@ wss.on('connection', (ws) => {
     });
 });
 
+// Lisää /status reitti
+app.get('/status', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error('Virhe luettaessa tiedostoa:', err);
+            res.status(500).send('Virhe luettaessa tiedostoa');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
 server.listen(port, () => {
     console.log(`Serveri käynnissä osoitteessa http://localhost:${port}`);
 });
