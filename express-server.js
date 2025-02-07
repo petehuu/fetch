2c31377964973d97f4e6035e940efdb05a2771b7
petehuu <petehuu@gmail.com>
2025-02-07 21:05:18 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
-const https = require('https');
-const WebSocket = require('ws');
+const http = require('http');
 
 const app = express();
-const port = 3000; // Käytetään porttia 3000
+const port = 4000; // Käytetään porttia 4000
 
 // Käytä cors-middlewarea
 app.use(cors());
@@ -14,42 +13,8 @@ app.use(cors());
 // Aseta staattiset tiedostot
 app.use(express.static(path.join(__dirname, '/')));
 
-// Lataa SSL-sertifikaatti ja avain
-const privateKey = fs.readFileSync('server.key', 'utf8');
-const certificate = fs.readFileSync('server.cert', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const server = https.createServer(credentials, app);
-
-// WebSocket-palvelin
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-    console.log('WebSocket-yhteys muodostettu');
-    ws.send(JSON.stringify({ message: 'Yhteys muodostettu' })); // Lähetetään vahvistus JSON-muodossa
-
-    ws.on('message', (message) => {
-        console.log('Vastaanotettu:', message);
-        // Käsittele WebSocket-viesti ja vastaa
-        if (message === 'getStatus') {
-            fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
-                if (err) {
-                    console.error('Virhe luettaessa tiedostoa:', err);
-                    ws.send(JSON.stringify({ status: 'error', message: 'Virhe luettaessa tiedostoa' }));
-                } else {
-                    ws.send(data);
-                }
-            });
-        }
-    });
-
-    ws.on('close', () => {
-        console.log('WebSocket-yhteys suljettu');
-    });
-});
-
-// Lisää `/status` reitti
-app.get('/status', (req, res) => {
+// Lisää `/api/status` reitti
+app.get('/api/status', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
         if (err) {
@@ -61,6 +26,8 @@ app.get('/status', (req, res) => {
     });
 });
 
+const server = http.createServer(app);
+
 server.listen(port, () => {
-    console.log(`Serveri käynnissä osoitteessa https://localhost:${port}`);
+    console.log(`Serveri käynnissä osoitteessa http://localhost:${port}`);
 });
