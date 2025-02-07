506dbed9bf85304da8dc2e4a41481513243cab37
petehuu <petehuu@gmail.com>
2025-02-07 17:34:49 +0200

    Create test-sockjs-client.js

---
+++ test-sockjs-client.js
@@ -0,0 +1,22 @@
+const SockJS = require('sockjs-client');
+
+const sock = new SockJS('http://localhost/websocket');
+
+sock.onopen = function() {
+    console.log('SockJS-yhteys avattu');
+    sock.send('getStatus');
+};
+
+sock.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    console.log('Vastaanotettu:', data);
+    sock.close();  // Sulje SockJS-yhteys viestin vastaanottamisen jälkeen
+};
+
+sock.onerror = function(e) {
+    console.error('SockJS-virhe:', e);
+};
+
+sock.onclose = function() {
+    console.log('SockJS-yhteys suljettu');
+};
