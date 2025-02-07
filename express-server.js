b471fe1f7765a3f882f78d55409b8338acdfac69
petehuu <petehuu@users.noreply.github.com>
2025-02-07 15:30:03 +0000

    deploy: a118676367078c633ae4d991520855f39e9a8af6

--- express-server.js
+++ express-server.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const http = require('http');
-const WebSocket = require('ws');
+const SockJS = require('sockjs');
 
 const app = express();
-const port = 80; // Muutettu portti
+const port = 80; // Käytetään porttia 80
 
 // Käytä cors-middlewarea
 app.use(cors());
@@ -14,50 +14,38 @@ app.use(cors());
 // Aseta staattiset tiedostot
 app.use(express.static(path.join(__dirname, '/')));
 
-// WebSocket-palvelin
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+// SockJS-palvelin
+const sockjs = SockJS.createServer();
+sockjs.on('connection', (conn) => {
+    console.log('SockJS-yhteys muodostettu');
+    conn.write('Yhteys muodostettu'); // Lähetetään vahvistus yhteydestä
 
-wss.on('connection', (ws) => {
-    console.log('WebSocket-yhteys muodostettu');
-    ws.send('Yhteys muodostettu'); // Lähetetään vahvistus yhteydestä
-
-    ws.on('message', (message) => {
+    conn.on('data', (message) => {
         console.log('Vastaanotettu:', message);
-        // Käsittele WebSocket-viesti ja vastaa
+        // Käsittele SockJS-viesti ja vastaa
         if (message === 'getStatus') {
             fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
                 if (err) {
                     console.error('Virhe luettaessa tiedostoa:', err);
-                    ws.send(JSON.stringify({ status: 'error', message: 'Virhe luettaessa tiedostoa' }));
+                    conn.write(JSON.stringify({ status: 'error', message: 'Virhe luettaessa tiedostoa' }));
                 } else {
-                    ws.send(data);
+                    conn.write(data);
                 }
             });
         }
     });
 
-    ws.on('error', (error) => {
-        console.error('WebSocket-virhe:', error);
+    conn.on('error', (error) => {
+        console.error('SockJS-virhe:', error);
     });
 
-    ws.on('close', (code, reason) => {
-        console.log(`WebSocket-yhteys suljettu. Koodi: ${code}, Syy: ${reason}`);
+    conn.on('close', () => {
+        console.log('SockJS-yhteys suljettu');
     });
 });
 
-// Lisää /status reitti
-app.get('/status', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    fs.readFile(path.join(__dirname, 'status.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.error('Virhe luettaessa tiedostoa:', err);
-            res.status(500).send('Virhe luettaessa tiedostoa');
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
-});
+const server = http.createServer(app);
+sockjs.installHandlers(server, { prefix: '/websocket' });
 
 server.listen(port, () => {
     console.log(`Serveri käynnissä osoitteessa http://localhost:${port}`);
